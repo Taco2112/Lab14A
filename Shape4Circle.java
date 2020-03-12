35792cca6a941edad00500a25ff7d6b19a6ef95f
TACO2112 <45360167+Taco2112@users.noreply.github.com>
2020-03-12 07:43:45 -0400

    SPLAT

--- Shape4Circle.java
+++ Shape4Circle.java
@@ -6,7 +6,36 @@ import java.awt.*;
 
 public class Shape4Circle implements Shape
 {
+   
+   public Shape4Circle(Graphics g)
+   {
+
+      /*drawShape(g);
+      displayNumSides(g);
+      displayName(g);*/
+      
+   }
+   
    public void drawShape(Graphics g)
    {
-   }   
+      
+      g.fillOval( 500 , 350 , 200 , 200 );
+      
+   }
+
+   public void displayNumSides(Graphics g)
+   {
+
+      g.drawString( "A Circle has 0 sides" , 420 , 580 );
+
+   }
+
+   public void displayName(Graphics g)
+   {
+
+      g.drawString( "Circle" , 420 , 320 );
+
+   }
+
+
 }
