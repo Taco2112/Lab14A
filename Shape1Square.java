4bad31cfd815b67cba5285c0c69a4b4a7663825f
TACO2112 <45360167+Taco2112@users.noreply.github.com>
2020-03-12 07:40:19 -0400

    SPLAT

--- Shape1Square.java
+++ Shape1Square.java
@@ -6,8 +6,38 @@ import java.awt.*;
 
 public class Shape1Square implements Shape
 {
+   
+   public Shape1Square(Graphics g)
+   {
+
+      /*drawShape(g);
+      displayNumSides(g);
+      displayName(g);*/
+      
+   }
+   
    public void drawShape(Graphics g)
    {
+      
+      g.fillRect( 105, 55, 200, 200 );
+      
    }
+
+   public void displayNumSides(Graphics g)
+   {
+
+      g.drawString( "A Square has 4 sides" , 20 , 280 );
+
+   }
+
+   public void displayName(Graphics g)
+   {
+
+      g.drawString( "Square" , 20 , 30 );
+
+   }
+   
+   
 }
+
 
